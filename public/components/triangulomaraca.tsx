63f3681c6b890a52c5e862c357e016d50ae90d4e
guilacerdas <guilacerda@gmail.com>
2023-04-29 11:40:23 -0300

    Exclui e renomeia componente

---
+++ public/components/triangulomaraca.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const TrianguloMaraca = () => {
+  return (
+    <>
+      <p>Pagina Triangulo e Maraca</p>
+    </>
+  );
+};
+
+export default TrianguloMaraca;
