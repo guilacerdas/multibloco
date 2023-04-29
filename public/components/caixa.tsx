3a46ea91791eed9df5fec74526fb9ccce5a62700
guilacerdas <guilacerda@gmail.com>
2023-04-29 11:41:32 -0300

    Adiciona botão fechar

--- public/components/caixa.tsx
+++ public/components/caixa.tsx
@@ -1,21 +1,34 @@
 import React, { useState } from "react";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
-const Caixa = () => {
+const Caixa = ({ showForm, setShowForm }: any) => {
   return (
     <>
-      <label htmlFor="pet-select">Escolha um ritmo:</label>
+      <section className="w-full border">
+        <div className="flex flex-col items-center gap-3 lg:flex-row lg:justify-center ">
+          <div className="flex w-full ">
+            <AiOutlineCloseCircle
+              className="text-2xl"
+              onClick={() => location.reload()}
+            />
+          </div>
+          <div className="flex ">
+            <label htmlFor="pet-select">Escolha um ritmo:</label>
 
-      <select
-        name="pets"
-        id="pet-select"
-        className="py-3 bg-white border rounded-md"
-      >
-        <option value="">--Escolha uma opção--</option>
-        <option value="baiao">Baião</option>
-        <option value="carimbo">Carimbó</option>
-        <option value="quadrilha">Quadrilha</option>
-        <option value="xote">Xote</option>
-      </select>
+            <select
+              name="pets"
+              id="pet-select"
+              className="py-3 bg-white border rounded-md"
+            >
+              <option value="">--Escolha uma opção--</option>
+              <option value="baiao">Baião</option>
+              <option value="carimbo">Carimbó</option>
+              <option value="quadrilha">Quadrilha</option>
+              <option value="xote">Xote</option>
+            </select>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
